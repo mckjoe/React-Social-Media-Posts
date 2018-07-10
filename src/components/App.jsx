083b3766e97c4ae7626a1c0ccf56c65c5daa9f96
Joe McKinney and Robin Sola <joe@>
2018-07-10 11:00:47 -0700

    create project skeleton

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from './Header'
+import { Switch, Route } from 'react-router-dom'
+import Error404 from './Error404'
+import { v4 } from 'uuid';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' render={()=><PostList />} />
+          <Route path='/newpost' render={()=><PostForm />}/>
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    )
+  }
+
+}
+
+export default App
